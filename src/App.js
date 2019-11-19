ef3c676cbc4949773fb81cb20e06140566f16f7a
Herman cheung <hcheung1985@gmail.com>
2019-11-19 16:24:08 -0500

    change app txt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hi there 222ww
+          Edit <code>src/App.js</code> There is life form here
         </p>
         <a
           className="App-link"
